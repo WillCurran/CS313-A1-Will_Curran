f29faf99a3ae9c97398c9fbab1884ed315a70db1
WillCurran <willecurran@gmail.com>
2019-09-19 10:08:50 -0500

    getting cmd line args

--- Main.cpp
+++ Main.cpp
@@ -1,57 +1,45 @@
 #include "Ackerman.h"
 #include "BuddyAllocator.h"
 #include "getopt.h"
+#include <unistd.h>
 
 int main(int argc, char ** argv) {
 
   int basic_block_size = 128, memory_length = 512 * 1024;
     
     int option;
-//    int bflg, aflg, errflg;
-//    char *ifile;
-//    char *ofile;
+//    int bflag = 0;
+//    int sflag = 0;
+    int errflag = 0;
     extern char *optarg;
     extern int optind, optopt;
+    
+    for(int i = 0; i < argc; i++) {
+        cout << argv[i] << endl;
+    }
 
-    while ((option = getopt(argc, argv, ":bs:")) != -1) {
+    while ((option = getopt(argc, argv, "b:s:")) != -1) {
         switch(option) {
             case 'b':
-//                basic_block_size = optopt;
-                cout << optarg << endl;
-//                if (bflg)
-//                    errflg++;
-//                else
-//                    aflg++;
+                if(isdigit(optarg[0]))
+                    basic_block_size = atoi(optarg);
+                else
+                    errflag++;
                 break;
             case 's':
-//                memory_length = optopt;
-                cout << optarg << endl;
-//                if (aflg)
-//                    errflg++;
-//                else {
-//                    bflg++;
-//                    bproc();
-//                }
-                break;
-            case ':':       /* -f or -o without operand */
-                fprintf(stderr,
-                        "Option -%c requires an operand\n", optopt);
-//                errflg++;
+                if(isdigit(optarg[0]))
+                    memory_length = atoi(optarg);
+                else
+                    errflag++;
                 break;
             case '?':
-                fprintf(stderr,
-                        "Unrecognized option: -%c\n", optopt);
-//                errflg++;
+                errflag++;
         }
     }
-//    if (errflg) {
-//        fprintf(stderr, "usage: . . . ");
-//        exit(2);
-//    }
-//    for ( ; optind < argc; optind++) {
-//        if (access(argv[optind], R_OK)) {
-//            . . .
-    
+    if (errflag) {
+        fprintf(stderr, "usage: memtest [-b <blocksize>] [-s <memsize>]\n");
+        return 1;
+    }
     
     
 //    BuddyAllocator * allocator = new BuddyAllocator(basic_block_size, memory_length);
